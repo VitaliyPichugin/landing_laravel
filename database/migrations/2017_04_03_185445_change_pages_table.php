dc1ca148c04b086d3c12819fb4b075c20a8ae96f
VitaliyPichugin <vexel1992@gmail.com>
2017-05-04 18:06:24 +0300

    Add files via upload

---
+++ database/migrations/2017_04_03_185445_change_pages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+
+            $table->renameColumn('img', 'images');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            //
+        });
+    }
+}
